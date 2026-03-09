e639f6a5a942ad59f07f4c3efd5eab732f40d0a8
Liwei Ji <jiliwei.phys@gmail.com>
2026-03-08 23:02:49 -0400

    KerrSchild: update kerrschild.cxx

--- KerrSchild/src/kerrschild.cxx
+++ KerrSchild/src/kerrschild.cxx
@@ -1,7 +1,6 @@
 #include <cctk.h>
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
-#include <cx_utils.hxx>
 #include <loop_device.hxx>
 
 #include <cmath>
@@ -12,19 +11,23 @@ namespace KerrSchild {
 using namespace Loop;
 using namespace CXUtils;
 
+CCTK_DEVICE CCTK_HOST constexpr CCTK_REAL Sqrt(CCTK_REAL x) {
+  return std::sqrt(x);
+}
+
 extern "C" void KerrSchild_InitialData(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_KerrSchild_InitialData;
   DECLARE_CCTK_PARAMETERS;
 
   const GF3D2layout layout2(cctkGH, {0, 0, 0});
 
-  const array<CCTK_REAL *, 6> gf_ADMgam{gxx, gxy, gxz, gyy, gyz, gzz};
-  const array<CCTK_REAL *, 6> gf_ADMK{kxx, kxy, kxz, kyy, kyz, kzz};
+  const std::array<CCTK_REAL *, 6> gf_ADMgam{gxx, gxy, gxz, gyy, gyz, gzz};
+  const std::array<CCTK_REAL *, 6> gf_ADMK{kxx, kxy, kxz, kyy, kyz, kzz};
   CCTK_REAL *gf_ADMalpha = alp;
-  const array<CCTK_REAL *, 3> gf_ADMbeta{betax, betay, betaz};
+  const std::array<CCTK_REAL *, 3> gf_ADMbeta{betax, betay, betaz};
 
-  CCTK_REAL *gf_dtADMalpha = dtalp;
-  const array<CCTK_REAL *, 3> gf_ADMdtbeta{dtbetax, dtbetay, dtbetaz};
+  CCTK_REAL *gf_ADMdtalpha = dtalp;
+  const std::array<CCTK_REAL *, 3> gf_ADMdtbeta{dtbetax, dtbetay, dtbetaz};
 
   const Loop::GridDescBaseDevice grid(cctkGH);
 
