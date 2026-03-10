8d8c3a2e75c8d3f0955bbba01e1ef0c92edf1de1
Liwei Ji <jiliwei.phys@gmail.com>
2026-03-10 13:55:36 -0400

    SingleBH: add Schwarzschild

--- SingleBH/src/singlebh.cxx
+++ SingleBH/src/singlebh.cxx
@@ -15,6 +15,32 @@ CCTK_DEVICE CCTK_HOST constexpr CCTK_REAL Sqrt(CCTK_REAL x) {
   return std::sqrt(x);
 }
 
+extern "C" void Schwarzschild_InitialData(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_Schwarzschild_InitialData;
+  DECLARE_CCTK_PARAMETERS;
+
+  const auto rs = 2 * mass;
+
+  grid.loop_all_device<0, 0, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const auto r = Sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+        auto f = (r > rs) ? (1 - rs / r) : epsilon;
+        const auto invf = 1.0 / f;
+
+        alp(p.I) = Sqrt(f);
+        betax(p.I) = 0.0;
+        betay(p.I) = 0.0;
+        betaz(p.I) = 0.0;
+        gxx(p.I) = 1.0 + (invf - 1.0) * (p.x * p.x) / (r * r);
+        gxy(p.I) = 0.0 + (invf - 1.0) * (p.x * p.y) / (r * r);
+        gxz(p.I) = 0.0 + (invf - 1.0) * (p.x * p.z) / (r * r);
+        gyy(p.I) = 1.0 + (invf - 1.0) * (p.y * p.y) / (r * r);
+        gyz(p.I) = 0.0 + (invf - 1.0) * (p.y * p.z) / (r * r);
+        gzz(p.I) = 1.0 + (invf - 1.0) * (p.z * p.z) / (r * r);
+      });
+}
+
 extern "C" void KerrSchild_InitialData(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_KerrSchild_InitialData;
   DECLARE_CCTK_PARAMETERS;
